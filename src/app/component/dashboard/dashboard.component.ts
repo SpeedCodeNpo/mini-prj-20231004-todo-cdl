add1fba5c2ab2c833270e5625a8ff8ccafa83b85
Yariv G <yariv.rivercybe@gmail.com>
2023-10-08 22:39:35 +0300

    The add task feature works

--- src/app/component/dashboard/dashboard.component.ts
+++ src/app/component/dashboard/dashboard.component.ts
@@ -1,10 +1,60 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { CrudService } from '../../service/crud.service';
+import { Task } from '../../model/task';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit{
 
-}
+  taskObj : Task = new Task();
+  taskArr : Task[] = [];
+
+  addTaskValue : string = '';
+
+  crudSvc = inject(CrudService);
+
+  ngOnInit(): void {
+    this.taskObj = new Task(); //resets the value of taskObj
+    this.getAllTaskComp(); //it loads a refreshed value for taskArr
+  }
+
+
+  addTaskComp(){
+    this.taskObj.task_name = this.addTaskValue;
+    this.crudSvc.addTask(this.taskObj).subscribe({
+      next: (value) => {
+        console.log(`Observer addTaskComp: Value of the emission is "${value}"`)
+        this.ngOnInit();
+        this.addTaskValue = '';
+      },
+
+      complete: () => console.log(`Observer addTaskComp: Triggered a COMPLETE.`),
+
+      error: (myError: Error) =>
+        console.log(
+          `Observer addTaskComp: Triggered an error, the message is : "${myError}"`
+        ),
+    });
+  }
+  
+  getAllTaskComp(): void {
+    this.crudSvc.getAllTask().subscribe({
+      next: (value) => {
+        console.log(`Observer getAllTaskComp: getAllTask returned value = "${value}"`)
+        this.taskArr = value;
+      },
+
+      complete: () => console.log(`Observer getAllTaskComp: Triggered a COMPLETE.`),
+
+      error: (myError: Error) =>
+        console.log(
+          `Observer getAllTaskComp: Triggered an error, the message is : "${myError}"`
+        ),
+    });
+  }
+
+}//end
+
